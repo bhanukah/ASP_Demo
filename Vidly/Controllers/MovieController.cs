4e8597f9f591d50130350cae731e87f419af8683
Bhanuka Hingalagoda <bhanuka.h93@gmail.com>
2019-08-08 00:43:00 +0930

    Ex3

--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,16 @@ namespace Vidly.Controllers
 {
     public class MovieController : Controller
     {
-        // GET: Movie
+        private ApplicationDbContext _context;
+        public MovieController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+            base.Dispose(disposing);
+        }
         public ActionResult Random()
         {
             Movie movie = new Movie() { Name = "Shrek!" };
@@ -48,7 +58,15 @@ namespace Vidly.Controllers
 
             //return Content(string.Format("page = {0}, sort by = {1}", pageIndex, sortBy));
 
-            return View(GetMovies());
+            return View(_context.Movie.Include(m => m.Genre));
+
+        }
+
+        public ActionResult Details(int id)
+        {
+            //return Content(string.Format("page = {0}, sort by = {1}", pageIndex, sortBy));
+
+            return View(_context.Movie.Include(m => m.Genre).SingleOrDefault(m => m.Id == id));
 
         }
 
@@ -56,15 +74,6 @@ namespace Vidly.Controllers
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(string.Format("y:{0} m:{1}", year, month));
-        }
-
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>()
-            {
-                new Movie() { Id = 1, Name = "Shrek!"},
-                new Movie() { Id = 2, Name = "Mad Max"}
-            };
         }
     }
 }
